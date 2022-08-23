9b72e6ead9f84e5be1dec8befea0b1ad52e25b26
yubin <fisherbin4@gmail.com>
2022-08-24 00:06:43 +0800

    feat: 组件通信

--- src/router/routes.js
+++ src/router/routes.js
@@ -13,6 +13,55 @@ import GroupOrder from "@/pages/Center/GroupOrder";
 
 export default [
     {
+        path: '/communication',
+        component: () => import('@/pages/Communication/Communication'),
+        children: [
+            {
+                path: 'event',
+                component: () => import('@/pages/Communication/EventTest/EventTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            },
+            {
+                path: 'model',
+                component: () => import('@/pages/Communication/ModelTest/ModelTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            },
+            {
+                path: 'sync',
+                component: () => import('@/pages/Communication/SyncTest/SyncTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            },
+            {
+                path: 'attrs-listeners',
+                component: () => import('@/pages/Communication/AttrsListenersTest/AttrsListenersTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            },
+            {
+                path: 'children-parent',
+                component: () => import('@/pages/Communication/ChildrenParentTest/ChildrenParentTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            },
+            {
+                path: 'scope-slot',
+                component: () => import('@/pages/Communication/ScopeSlotTest/ScopeSlotTest'),
+                meta: {
+                    isHideFooter: true
+                },
+            }
+        ],
+    },
+
+    {
         name: 'center',
         path: '/center',
         component: () => import("@/pages/Center"), // 路由懒加载, 在使用组件时加载js
